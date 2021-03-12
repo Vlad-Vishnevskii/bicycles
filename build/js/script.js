7b19711da27646187d0dda313c931bc07fbf2584
Vlad <vishnevskii_vlad@mail.ru>
2021-03-12 12:18:53 +0300

    Исправляет верстку шапки для мобильных версий

--- build/js/script.js
+++ build/js/script.js
@@ -1,22 +1,22 @@
 'use strict';
-const mainNav = document.querySelector(`.main-nav`);
-const mainNavClose = mainNav.querySelector(`.main-nav__close`);
-const burgerButton = document.querySelector(`.mobile-header__button`);
+var mainNav = document.querySelector('.main-nav');
+var mainNavClose = mainNav.querySelector('.main-nav__close');
+var burgerButton = document.querySelector('.mobile-top__button');
 
-mainNav.classList.remove(`main-nav_no-js`);
-mainNavClose.classList.remove(`main-nav__close_no-js`);
-burgerButton.classList.remove(`mobile-header__button_no-js`);
+mainNav.classList.remove('main-nav_no-js');
+mainNavClose.classList.remove('main-nav__close_no-js');
+burgerButton.classList.remove('mobile-top__button_no-js');
 
-const closeMenu = function () {
-  mainNav.classList.remove(`main-nav_open`)
-  mainNavClose.removeEventListener(`click`, closeMenu)
+var closeMenu = function () {
+  mainNav.classList.remove('main-nav_open');
+  mainNavClose.removeEventListener('click', closeMenu);
 };
 
-const openMenu = function () {
-  if (!mainNav.classList.contains(`main-nav_open`)) {
-    mainNav.classList.add(`main-nav_open`)
-    mainNavClose.addEventListener(`click`, closeMenu)
+var openMenu = function () {
+  if (!mainNav.classList.contains('main-nav_open')) {
+    mainNav.classList.add('main-nav_open');
+    mainNavClose.addEventListener('click', closeMenu);
   }
 };
 
-burgerButton.addEventListener(`click`, openMenu);
+burgerButton.addEventListener('click', openMenu);
