aa6de2f563ee1da40b1ef45021e051bd336f3b32
Vlad <vishnevskii_vlad@mail.ru>
2021-03-19 23:50:43 +0300

    Исправляет блок контейнер

--- source/js/script.js
+++ source/js/script.js
@@ -1,28 +1,27 @@
+'use strict';
 (function () {
 
-'use strict';
-var page = document.querySelector('.page-body');
-var mainNav = document.querySelector('.main-nav');
-var closeButton = document.querySelector('.main-nav__close-btn');
-var burgerButton = document.querySelector('.mobile-top__button');
+  var mainNav = document.querySelector('.main-nav');
+  var closeButton = document.querySelector('.main-nav__close-btn');
+  var burgerButton = document.querySelector('.mobile-top__button');
 
-var closeMenu = function () {
-  mainNav.classList.remove('main-nav_open');
-  closeButton.removeEventListener('click', closeMenu);
-};
+  var closeMenu = function () {
+    mainNav.classList.remove('main-nav_open');
+    closeButton.removeEventListener('click', closeMenu);
+  };
 
-var openMenu = function () {
-  if (!mainNav.classList.contains('main-nav_open')) {
-    mainNav.classList.add('main-nav_open');
-    closeButton.addEventListener('click', closeMenu);
+  var openMenu = function () {
+    if (!mainNav.classList.contains('main-nav_open')) {
+      mainNav.classList.add('main-nav_open');
+      closeButton.addEventListener('click', closeMenu);
+    }
+  };
+
+  if (mainNav && closeButton && burgerButton) {
+    mainNav.classList.remove('main-nav_no-js');
+    closeButton.classList.remove('main-nav__close-btn_no-js');
+    burgerButton.classList.remove('mobile-top__button_no-js');
+    burgerButton.addEventListener('click', openMenu);
   }
-};
-
-if (mainNav && closeButton && burgerButton) {
-  mainNav.classList.remove('main-nav_no-js');
-  closeButton.classList.remove('main-nav__close-btn_no-js');
-  burgerButton.classList.remove('mobile-top__button_no-js');
-  burgerButton.addEventListener('click', openMenu);
-};
 
 })();
